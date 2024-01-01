527d177447211fca4c4340d77b70f076cf5fc7ca
rahul0901 <rahul.shinde0904@gmail.com>
2024-01-02 00:16:37 +0530

    add

--- todofrontend/src/Helpers/Axios.Config.js
+++ todofrontend/src/Helpers/Axios.Config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 var BackendURL = 'http://localhost:8000/api/v1/';
 
 if(process.env.Mode === "PRODUCTION"){
-    BackendURL = "";
+    BackendURL = "https://todobackend-sg4c.onrender.com/api/v1";
 }
 
 const api = axios.create({baseURL: BackendURL})
